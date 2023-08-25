fdafc725d4076c0b570ea8f41d8b2d92303e8a91
Universumgames <programming@universegame.de>
2023-08-25 15:50:31 +0200

    update apple site association

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,7 @@ const failedLoading = ref(false)
 const isImpersonating = ref(false)
 const impersonatingMail = ref("")
 
-const frontendVersion = ref("2.9.4")
+const frontendVersion = ref("3.0.0")
 const backendVersion = ref("unknown")
 const clientIsCompatible = ref(true)
 
